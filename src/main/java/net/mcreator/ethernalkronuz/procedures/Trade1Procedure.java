57b548467e03848f0113208730aa9b198ced4f25
Guilherme-Bernardino <202001870@estudantes.ips.pt>
2024-11-05 15:51:29 +0000

    Tecnhobalde God Apple trade

--- src/main/java/net/mcreator/ethernalkronuz/procedures/Trade1Procedure.java
+++ src/main/java/net/mcreator/ethernalkronuz/procedures/Trade1Procedure.java
@@ -18,8 +18,8 @@ public class Trade1Procedure {
 		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY).getItem() == EthernalKronuzModItems.BATTLE_COIN
 				.get()) {
 			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
-				ItemStack _setstack = new ItemStack(Items.GOLDEN_APPLE);
-				_setstack.setCount(64);
+				ItemStack _setstack = new ItemStack(Items.ENCHANTED_GOLDEN_APPLE);
+				_setstack.setCount(1);
 				((Slot) _slots.get(1)).set(_setstack);
 				_player.containerMenu.broadcastChanges();
 			}
