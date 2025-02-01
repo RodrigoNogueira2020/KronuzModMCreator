34efc8aba2320aec8860daf790e2c1ad0a313705
Rodrigo Nogueira <62758689+RodrigoNogueira2020@users.noreply.github.com>
2025-02-01 01:05:21 +0000

    As armas ficam agora presas no inventário do jogador
    Não é possível arrastar para baús, shulkers, etc etc. É possivel usar os números para settar o slot da hotbar que a arma vai ficar

--- src/main/java/net/mcreator/ethernalkronuz/PreventDropEvent.java
+++ src/main/java/net/mcreator/ethernalkronuz/PreventDropEvent.java
@@ -1,13 +1,14 @@
 package net.mcreator.ethernalkronuz;
 
 import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.event.entity.player.PlayerContainerEvent;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.Slot;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 import net.mcreator.ethernalkronuz.init.EthernalKronuzModItems;
 
 @Mod.EventBusSubscriber
@@ -27,21 +28,35 @@ public class PreventDropEvent {
     }
 
     @SubscribeEvent
+    public static void onMouseClicked(ScreenEvent.MouseClickedEvent event) {
+        if (!(event.getScreen() instanceof net.minecraft.client.gui.screens.inventory.AbstractContainerScreen<?>))
+            return;
+
+        Player player = Minecraft.getInstance().player;
+
+        if (player != null && !player.isCreative()) {
+            Slot clickedSlot = ((net.minecraft.client.gui.screens.inventory.AbstractContainerScreen<?>) event.getScreen()).getSlotUnderMouse();
+            if (clickedSlot != null && clickedSlot.hasItem()) {
+                ItemStack clickedItem = clickedSlot.getItem();
+                if (isRestrictedItem(clickedItem)) {
+                    event.setCanceled(true);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
     public static void onContainerClose(PlayerContainerEvent.Close event) {
         Player player = event.getPlayer();
 
         if (player.isCreative())
             return;
 
-        AbstractContainerMenu containerMenu = player.containerMenu;
-
-        if (containerMenu != null) {
-            for (Slot slot : containerMenu.slots) {
-            	
-                if (slot.hasItem()) {
-                    ItemStack stack = slot.getItem();
-                    
-                    if (isRestrictedItem(stack)) {
+        for (Slot slot : player.containerMenu.slots) {
+            if (slot.hasItem()) {
+                ItemStack stack = slot.getItem();
+                if (isRestrictedItem(stack)) {
+                    if (!player.getInventory().contains(stack)) {
                         slot.set(ItemStack.EMPTY);
                         player.getInventory().add(stack);
                     }
