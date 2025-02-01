ed397fc10a93e95d4298af53cf6bb706ff16289b
Rodrigo Nogueira <62758689+RodrigoNogueira2020@users.noreply.github.com>
2025-02-01 21:52:05 +0000

    É dado um Set novo de RL ao respawnar, caso o RL morrer

---
+++ src/main/java/net/mcreator/ethernalkronuz/procedures/GiveRLStuffAfterRespawnProcedure.java
@@ -0,0 +1,198 @@
+package net.mcreator.ethernalkronuz.procedures;
+
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.network.chat.TextComponent;
+
+import net.mcreator.ethernalkronuz.network.EthernalKronuzModVariables;
+import net.mcreator.ethernalkronuz.init.EthernalKronuzModMobEffects;
+import net.mcreator.ethernalkronuz.init.EthernalKronuzModItems;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class GiveRLStuffAfterRespawnProcedure {
+	@SubscribeEvent
+	public static void onPlayerRespawned(PlayerEvent.PlayerRespawnEvent event) {
+		execute(event, event.getPlayer());
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables())).IsRadiantLord) {
+			if (entity instanceof Player _player) {
+				_player.getAbilities().mayfly = (true);
+				_player.onUpdateAbilities();
+			}
+			{
+				Entity _ent = entity;
+				if (!_ent.level.isClientSide() && _ent.getServer() != null)
+					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "/attribute @p minecraft:generic.max_health base set 40");
+			}
+			if (entity instanceof LivingEntity _entity)
+				_entity.addEffect(new MobEffectInstance(EthernalKronuzModMobEffects.RADIANT_LORD_EFFECT.get(), (int) Double.POSITIVE_INFINITY, 0, (true), (false)));
+		}
+		if ((entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables())).RadiantLordRoxoPlayer) {
+			entity.setCustomName(new TextComponent("Gargantuan"));
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(EthernalKronuzModItems.BLADE_OF_THE_VOID_SETUP.get());
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(EthernalKronuzModItems.JOTUNHEIM.get());
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(3, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_HELMET.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.HEAD, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_HELMET.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(2, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_CHESTPLATE.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.CHEST, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_CHESTPLATE.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(1, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_LEGGINGS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_LEGGINGS.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(0, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_BOOTS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_BOOTS.get()));
+				}
+			}
+		}
+		if ((entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables())).RadiantLordVerdePlayer) {
+			entity.setCustomName(new TextComponent("Azakana"));
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(EthernalKronuzModItems.TERRA_BLADE_SETUP.get());
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(EthernalKronuzModItems.JOTUNHEIM.get());
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(3, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_HELMET.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.HEAD, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_HELMET.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(2, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_CHESTPLATE.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.CHEST, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_CHESTPLATE.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(1, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_LEGGINGS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_LEGGINGS.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(0, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_BOOTS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_BOOTS.get()));
+				}
+			}
+		}
+		if ((entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables())).RadiantLordVermelhoPlayer) {
+			entity.setCustomName(new TextComponent("Titan Kaleb"));
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(EthernalKronuzModItems.MURASAMA.get());
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(EthernalKronuzModItems.JOTUNHEIM.get());
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(3, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_HELMET.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.HEAD, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_HELMET.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(2, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_CHESTPLATE.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.CHEST, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_CHESTPLATE.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(1, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_LEGGINGS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_LEGGINGS.get()));
+				}
+			}
+			{
+				Entity _entity = entity;
+				if (_entity instanceof Player _player) {
+					_player.getInventory().armor.set(0, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_BOOTS.get()));
+					_player.getInventory().setChanged();
+				} else if (_entity instanceof LivingEntity _living) {
+					_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_BOOTS.get()));
+				}
+			}
+		}
+	}
+}
