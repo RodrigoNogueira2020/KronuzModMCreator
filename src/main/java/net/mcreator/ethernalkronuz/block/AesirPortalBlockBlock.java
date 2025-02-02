46d1843bc6e7efe73c7aaa7da593860c01110f43
GreyBoyDev <202001870@estudantes.ips.pt>
2025-02-02 19:28:17 +0000

    Nova textura para o portal de Asgard

--- src/main/java/net/mcreator/ethernalkronuz/block/AesirPortalBlockBlock.java
+++ src/main/java/net/mcreator/ethernalkronuz/block/AesirPortalBlockBlock.java
@@ -10,12 +10,18 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Mob;
@@ -32,8 +38,11 @@ import java.util.List;
 import java.util.Collections;
 
 public class AesirPortalBlockBlock extends Block {
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
 	public AesirPortalBlockBlock() {
 		super(BlockBehaviour.Properties.of(Material.PORTAL).sound(SoundType.STONE).strength(-1, 3600000).lightLevel(s -> 15).noCollission().randomTicks().hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 
 	@Override
@@ -49,6 +58,24 @@ public class AesirPortalBlockBlock extends Block {
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 0;
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+	}
+
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+	}
+
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
 	}
 
 	@Override
