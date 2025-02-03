fe93695bb55172f25df87178cf200e946fe7f62b
GreyBoyDev <202001870@estudantes.ips.pt>
2025-02-03 15:53:38 +0000

    Bifrost Key do Curios abre o portal

--- src/main/java/net/mcreator/ethernalkronuz/item/BifrostKeyItem.java
+++ src/main/java/net/mcreator/ethernalkronuz/item/BifrostKeyItem.java
@@ -4,20 +4,46 @@ package net.mcreator.ethernalkronuz.item;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.core.BlockPos;
 
 import net.mcreator.ethernalkronuz.init.EthernalKronuzModTabs;
+import net.mcreator.ethernalkronuz.block.JotunheimPortalBlock;
 
 import java.util.List;
 
 public class BifrostKeyItem extends Item implements ICurioItem {
 	public BifrostKeyItem() {
 		super(new Item.Properties().tab(EthernalKronuzModTabs.TAB_CREATIVE_TAB).stacksTo(1).rarity(Rarity.EPIC));
+	}
+
+	public InteractionResult useOn(UseOnContext context) {
+		Player entity = context.getPlayer();
+		BlockPos pos = context.getClickedPos().relative(context.getClickedFace());
+		ItemStack itemstack = context.getItemInHand();
+		Level world = context.getLevel();
+		if (!entity.mayUseItemAt(pos, context.getClickedFace(), itemstack)) {
+			return InteractionResult.FAIL;
+		} else {
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			boolean success = false;
+			if (world.isEmptyBlock(pos) && true) {
+				JotunheimPortalBlock.portalSpawn(world, pos);
+				itemstack.hurtAndBreak(1, entity, c -> c.broadcastBreakEvent(context.getHand()));
+				success = true;
+			}
+			return success ? InteractionResult.SUCCESS : InteractionResult.FAIL;
+		}
 	}
 
 	@Override
