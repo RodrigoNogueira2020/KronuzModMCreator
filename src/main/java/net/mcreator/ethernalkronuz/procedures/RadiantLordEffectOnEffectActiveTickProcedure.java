fb04750bec24f499b0307501187694bdc92d5df8
Rodrigo Nogueira <62758689+RodrigoNogueira2020@users.noreply.github.com>
2025-02-09 19:18:10 +0000

    BIFROST_KEY já não desaparece quando se clica na tecla Q mousehouver

--- src/main/java/net/mcreator/ethernalkronuz/procedures/RadiantLordEffectOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/ethernalkronuz/procedures/RadiantLordEffectOnEffectActiveTickProcedure.java
@@ -1,5 +1,7 @@
 package net.mcreator.ethernalkronuz.procedures;
 
+import top.theillusivec4.curios.api.CuriosApi;
+
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,42 +15,59 @@ public class RadiantLordEffectOnEffectActiveTickProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof Player _player) {
-			// Remover duplicatas do inventário
-			for (int i = 0; i < _player.getInventory().items.size(); i++) {
-				ItemStack stack = _player.getInventory().items.get(i);
-				if (stack.getItem() == EthernalKronuzModItems.RL_VERDE_ARMOR_HELMET.get() || stack.getItem() == EthernalKronuzModItems.RL_VERDE_ARMOR_CHESTPLATE.get() || stack.getItem() == EthernalKronuzModItems.RL_VERDE_ARMOR_LEGGINGS.get()
-						|| stack.getItem() == EthernalKronuzModItems.RL_VERDE_ARMOR_BOOTS.get() || stack.getItem() == EthernalKronuzModItems.RL_ROXO_ARMOUR_HELMET.get() || stack.getItem() == EthernalKronuzModItems.RL_ROXO_ARMOUR_CHESTPLATE.get()
-						|| stack.getItem() == EthernalKronuzModItems.RL_ROXO_ARMOUR_LEGGINGS.get() || stack.getItem() == EthernalKronuzModItems.RL_ROXO_ARMOUR_BOOTS.get() || stack.getItem() == EthernalKronuzModItems.RL_VERMELHO_ARMOR_HELMET.get()
-						|| stack.getItem() == EthernalKronuzModItems.RL_VERMELHO_ARMOR_CHESTPLATE.get() || stack.getItem() == EthernalKronuzModItems.RL_VERMELHO_ARMOR_LEGGINGS.get()
-						|| stack.getItem() == EthernalKronuzModItems.RL_VERMELHO_ARMOR_BOOTS.get()) {
-					_player.getInventory().items.set(i, ItemStack.EMPTY);
+		EthernalKronuzModVariables.PlayerVariables playerVars = entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables());
+		if (playerVars.RadiantLordVerdePlayer) {
+			equipArmorSet(entity, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_HELMET.get()), new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_CHESTPLATE.get()), new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_LEGGINGS.get()),
+					new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_BOOTS.get()));
+		}
+		if (playerVars.RadiantLordRoxoPlayer) {
+			equipArmorSet(entity, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_HELMET.get()), new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_CHESTPLATE.get()), new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_LEGGINGS.get()),
+					new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_BOOTS.get()));
+		}
+		if (playerVars.RadiantLordVermelhoPlayer) {
+			equipArmorSet(entity, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_HELMET.get()), new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_CHESTPLATE.get()), new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_LEGGINGS.get()),
+					new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_BOOTS.get()));
+		}
+		forceEquipCurio(entity, new ItemStack(EthernalKronuzModItems.BIFROST_KEY.get()));
+	}
+
+	private static void equipArmorSet(Entity entity, ItemStack helmet, ItemStack chestplate, ItemStack leggings, ItemStack boots) {
+		forceEquipArmor(entity, helmet, EquipmentSlot.HEAD);
+		forceEquipArmor(entity, chestplate, EquipmentSlot.CHEST);
+		forceEquipArmor(entity, leggings, EquipmentSlot.LEGS);
+		forceEquipArmor(entity, boots, EquipmentSlot.FEET);
+	}
+
+	private static void forceEquipArmor(Entity entity, ItemStack item, EquipmentSlot slot) {
+		if (entity instanceof Player player) {
+			if (player.getInventory().armor.get(slot.getIndex()).isEmpty()) {
+				player.getInventory().armor.set(slot.getIndex(), item);
+				player.getInventory().setChanged();
+			}
+		} else if (entity instanceof LivingEntity living) {
+			if (living.getItemBySlot(slot).isEmpty()) {
+				living.setItemSlot(slot, item);
+			}
+		}
+	}
+
+	private static void forceEquipCurio(Entity entity, ItemStack item) {
+		if (entity instanceof Player player) {
+			CuriosApi.getCuriosHelper().getCuriosHandler(player).ifPresent(handler -> {
+				boolean hasCurio = handler.getCurios().values().stream().anyMatch(stackHandler -> stackHandler.getStacks().getSlots() > 0 && !stackHandler.getStacks().getStackInSlot(0).isEmpty());
+				if (!hasCurio) {
+					handler.getCurios().values().stream().filter(stackHandler -> stackHandler.getStacks().getSlots() > 0 && stackHandler.getStacks().getStackInSlot(0).isEmpty()).findFirst().ifPresent(stackHandler -> {
+						stackHandler.getStacks().setStackInSlot(0, item);
+						for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
+							ItemStack stack = player.getInventory().getItem(i);
+							if (stack.getItem() == EthernalKronuzModItems.BIFROST_KEY.get() && stack.getCount() > 0) {
+								stack.shrink(1);
+								break;
+							}
+						}
+					});
 				}
-			}
-		}
-		if ((entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables())).RadiantLordVerdePlayer) {
-			if (entity instanceof LivingEntity _living) {
-				_living.setItemSlot(EquipmentSlot.HEAD, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_HELMET.get()));
-				_living.setItemSlot(EquipmentSlot.CHEST, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_CHESTPLATE.get()));
-				_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_LEGGINGS.get()));
-				_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(EthernalKronuzModItems.RL_VERDE_ARMOR_BOOTS.get()));
-			}
-		}
-		if ((entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables())).RadiantLordRoxoPlayer) {
-			if (entity instanceof LivingEntity _living) {
-				_living.setItemSlot(EquipmentSlot.HEAD, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_HELMET.get()));
-				_living.setItemSlot(EquipmentSlot.CHEST, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_CHESTPLATE.get()));
-				_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_LEGGINGS.get()));
-				_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(EthernalKronuzModItems.RL_ROXO_ARMOUR_BOOTS.get()));
-			}
-		}
-		if ((entity.getCapability(EthernalKronuzModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new EthernalKronuzModVariables.PlayerVariables())).RadiantLordVermelhoPlayer) {
-			if (entity instanceof LivingEntity _living) {
-				_living.setItemSlot(EquipmentSlot.HEAD, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_HELMET.get()));
-				_living.setItemSlot(EquipmentSlot.CHEST, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_CHESTPLATE.get()));
-				_living.setItemSlot(EquipmentSlot.LEGS, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_LEGGINGS.get()));
-				_living.setItemSlot(EquipmentSlot.FEET, new ItemStack(EthernalKronuzModItems.RL_VERMELHO_ARMOR_BOOTS.get()));
-			}
+			});
 		}
 	}
 }
