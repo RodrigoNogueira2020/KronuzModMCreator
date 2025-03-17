e122c09f69a77eb5dff61e6df9f9ab887f617855
Rodrigo Nogueira <62758689+RodrigoNogueira2020@users.noreply.github.com>
2025-03-17 17:46:34 +0000

    End Ruins Corrigido

--- src/main/java/net/mcreator/ethernalkronuz/procedures/EndruinsmediumAdditionalGenerationConditionProcedure.java
+++ src/main/java/net/mcreator/ethernalkronuz/procedures/EndruinsmediumAdditionalGenerationConditionProcedure.java
@@ -4,43 +4,44 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemp
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.ethernalkronuz.init.EthernalKronuzModBlocks;
+
 public class EndruinsmediumAdditionalGenerationConditionProcedure {
 	public static boolean execute(LevelAccessor world, double x, double y, double z) {
-		boolean found = false;
-		double sx = 0;
-		double sy = 0;
-		double sz = 0;
-		sx = -3;
-		found = false;
-		for (int index0 = 0; index0 < (int) (6); index0++) {
-			sy = -3;
-			for (int index1 = 0; index1 < (int) (6); index1++) {
-				sz = -3;
-				for (int index2 = 0; index2 < (int) (6); index2++) {
-					if (!((world.getBlockState(new BlockPos(x + sx, y + sy, z + sz))).getBlock() == Blocks.AIR || (world.getBlockState(new BlockPos(x + sx, y + sy, z + sz))).getBlock() == Blocks.VOID_AIR
-							|| (world.getBlockState(new BlockPos(x + sx, y + sy, z + sz))).getBlock() == Blocks.CAVE_AIR)) {
-						found = true;
+		Block[] allowedBlocks = {EthernalKronuzModBlocks.ASGARDIAN_DIRT.get(), EthernalKronuzModBlocks.ASGARDIAN_DIRT_BLOCK.get(), EthernalKronuzModBlocks.DIVINE_CRYSTAL.get(), EthernalKronuzModBlocks.HEAVEN_STONE.get()};
+		int sizeX = 12;
+		int sizeZ = 12;
+		int sizeY = 3;
+		for (int dx = 0; dx < sizeX; dx++) {
+			for (int dz = 0; dz < sizeZ; dz++) {
+				for (int dy = 0; dy < sizeY; dy++) {
+					BlockPos checkPos = new BlockPos(x + dx, y + dy, z + dz);
+					Block currentBlock = world.getBlockState(checkPos).getBlock();
+					boolean isValid = false;
+					for (Block allowed : allowedBlocks) {
+						if (currentBlock == allowed) {
+							isValid = true;
+							break;
+						}
 					}
-					sz = sz + 1;
-				}
-				sy = sy + 1;
-			}
-			sx = sx + 1;
-		}
-		if (found == true) {
-			if (world instanceof ServerLevel _serverworld) {
-				StructureTemplate template = _serverworld.getStructureManager().getOrCreate(new ResourceLocation("ethernal_kronuz", "end_ruins_medium"));
-				if (template != null) {
-					template.placeInWorld(_serverworld, new BlockPos(x, y, z), new BlockPos(x, y, z), new StructurePlaceSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setIgnoreEntities(false), _serverworld.random, 3);
+					if (!isValid) {
+						return false;
+					}
 				}
 			}
 		}
-		return found;
+		if (world instanceof ServerLevel _serverworld) {
+			StructureTemplate template = _serverworld.getStructureManager().getOrCreate(new ResourceLocation("ethernal_kronuz", "end_ruins_large"));
+			if (template != null) {
+				template.placeInWorld(_serverworld, new BlockPos(x, y, z), new BlockPos(x, y, z), new StructurePlaceSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setIgnoreEntities(false), _serverworld.random, 3);
+			}
+		}
+		return true;
 	}
 }
